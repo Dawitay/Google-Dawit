31e9afd9a8f2fc22033dbf335d111fecb3211b16
Dawitay <dawitayelewoldesenbet@gmail.com>
2025-11-10 22:30:10 +0300

    Create search page for image and web

---
+++ src/app/search/layout.jsx
@@ -0,0 +1,36 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./../globals.css";
+import SearchHeader from "@/components/SearchHeader";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Google Dawit",
+  description: "Search Engine created by Dawit Ayele",
+  icons: {
+    icon: "/google.svg", // Path to your favicon in the public folder
+  },
+  
+};
+
+export default function SearchLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <SearchHeader/>
+        {children}
+
+      </body>
+    </html>
+  );
+}
